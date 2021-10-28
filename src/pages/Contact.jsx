bf874379b51ba08a80d11708bd7602b4b32056c4
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Air.local>
2021-10-28 21:38:50 +0600

    upate login page and contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,62 @@
 import React from 'react';
-
+import { Button,  Form, Row, Col} from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 const Contact = () => {
+
+    const navigation = useHistory();
+
+    const addEmail = e => {
+        e.preventDefault();
+        navigation.push( '/addblogverification' );
+    }
     return (
-        <div>
+        <Row className="d-flex" style={{backgroundColor:"#1C75BA", height:"600px", marginBottom:"50px", color:"#fff"}}>
            
-            This is contact
-          
-        </div>
+            <Col className="container p-5 xs={12} md={2}">
+                <h1  >We are avaliable</h1>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <h5 className="fw-lighter " >Mon to Fri : 9 am to 5 pm</h5>
+                <h5 className="fw-lighter" >Sat : On special appointment</h5>
+            </Col>
+            <Col className="xs={12} md={2} container mt-2 p-5">
+            <Form onSubmit={ addEmail }>
+                <Row className="d-flex ">
+                    <Col className="xs={12} md={4} ">
+                            <Form.Group className="mb-3 w-75 shadow rounded  "  controlId="blogForm.ControlInput1">
+                                <Form.Control type="text" placeholder="First name" className="p-3" />
+                            </Form.Group>
+                            </Col>
+                            <Col className="xs={12} md={4} ">
+                            <Form.Group className="mb-3 w-75 shadow rounded "  controlId="blogForm.ControlInput1">
+                                <Form.Control type="text" placeholder="Middle name" className="p-3" />
+                            </Form.Group>
+                            </Col>
+
+                            <Col className="xs={12} md={4} ">
+                            <Form.Group className="mb-3 w-75 shadow rounded " controlId="blogForm.ControlInput1">
+                                <Form.Control type="text" placeholder="Last name" className="p-3" />
+                            </Form.Group>
+                            </Col>
+                        </Row>
+                            <Form.Group className="mb-3 shadow rounded  " controlId="blogForm.ControlInput1">
+                                <Form.Control type="text" placeholder="Email" className="p-3" />
+                            </Form.Group>
+                            <Form.Group className="mb-3 shadow rounded " controlId="blogForm.ControlInput1">
+                                <Form.Control as="textarea" rows={ 10 } placeholder="Description" />
+                                
+                            </Form.Group>
+                            
+                            
+
+                            <Button variant="warning" type="submit" className="px-5 py-3 fw-bold shadow rounded ">Submit</Button>
+                        </Form>
+                        </Col>
+        </Row>
     )
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
-
+import { Button,  Form, Row, Col} from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
+
+    const navigation = useHistory();
+
+    const addLogin = e => {
+        e.preventDefault();
+        navigation.push( '/addblogverification' );
+    }
     return (
-        <div>
+        <Row  className="d-flex" style={{backgroundColor:"#1C75BA", height:"600px", marginBottom:"50px", color:"#fff"}}>
            
-            this is Login
+            <Col className="d-flex text-center mt-5 p-5 ">
+                <hr /> <p className="fw-lighter "> or continue with</p> <hr/>
+            </Col>
+            <Col className="container mt-5 p-5">
+                <h1> Sign in to your account</h1>
+                <p className="fw-lighter"> of if you don't have an account click on Sign up</p>
+                <Form onSubmit={ addLogin }>
+                            <Form.Group className="mb-3 shadow rounded " controlId="blogForm.ControlInput1">
+                                <Form.Control type="text" placeholder="user name or email" className="p-3" />
+                            </Form.Group>
+                            <Form.Group className="mb-3 shadow rounded " controlId="blogForm.ControlInput1">
+                                <Form.Control type="password" placeholder="password" className="p-3" />
+                            </Form.Group>
+                            
+                            
+
+                            <Button variant="warning" type="submit" className="px-5 py-3 shadow rounded fw-bold">Login</Button>
+                        </Form>
+            </Col>
            
-        </div>
+        </Row> 
     );
 };
 
