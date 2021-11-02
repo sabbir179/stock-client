d3af1e1a808a62190b33fb672b7230757dc140f0
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Air.local>
2021-11-02 06:08:44 +0600

    change stock number colore

--- src/components/Home/Stock.js
+++ src/components/Home/Stock.js
@@ -7,58 +7,58 @@ const Stock = () => {
         <div className="test">
             <div style={{margin: "10px 20px"}} >
                 <h4 className="fw-light text-primary" >AAPL</h4>
-                <p className="fw-bold text-success" >148.96</p>
+                <p className="fw-bold text-dark" >148.96</p>
                 <p className="fw-bold text-danger">-0.84 (-0.56%) </p>
             </div>
             <div>
                 <h4 className="fw-light text-primary" >S&P 500</h4>
-                <p className="fw-bold text-success">4,613.67</p>
-                <p className="fw-bold text-danger">+8.29(+0.18%)</p>
+                <p className="fw-bold text-dark">4,613.67</p>
+                <p className="fw-bold text-success">+8.29(+0.18%)</p>
             </div>
             <div>
                 <h4 className="fw-light text-primary">Dow 30</h4>
-                <p className="fw-bold text-success">35,913.84</p>
-                <p className="fw-bold text-danger">+94.28(+0.26%)</p>
+                <p className="fw-bold text-dark">35,913.84</p>
+                <p className="fw-bold text-success">+94.28(+0.26%)</p>
             </div>
             <div>
                 <h4 className="fw-light text-primary">Nasdaq</h4>
-                <p className="fw-bold text-success">15,595.92</p>
-                <p className="fw-bold text-danger">+97.53(+0.63%)</p>
+                <p className="fw-bold text-dark">15,595.92</p>
+                <p className="fw-bold text-success">+97.53(+0.63%)</p>
             </div>
             <div>
                 <h4 className="fw-light text-primary">Russell 2000</h4>
-                <p className="fw-bold text-success">2,358.12</p>
-                <p className="fw-bold text-danger">+60.93(+2.65%)</p>
+                <p className="fw-bold text-dark">2,358.12</p>
+                <p className="fw-bold text-success">+60.93(+2.65%)</p>
             </div>
             <div>
                 <h4 className="fw-light text-primary">Crude Oil</h4>
-                <p className="fw-bold text-success">83.90</p>
-                <p className="fw-bold text-danger">+0.33(+0.39%)</p>
+                <p className="fw-bold text-dark">83.90</p>
+                <p className="fw-bold text-success">+0.33(+0.39%)</p>
             </div>
             <div>
                 <h4 className="fw-light text-primary">Gold</h4>
-                <p className="fw-bold text-success">1,794.50</p>
-                <p className="fw-bold text-danger">+10.60(+0.59%)</p>
+                <p className="fw-bold text-dark">1,794.50</p>
+                <p className="fw-bold text-success">+10.60(+0.59%)</p>
             </div>
             <div>
                 <h4 className="fw-light text-primary">Silver</h4>
-                <p className="fw-bold text-success">24.12</p>
-                <p className="fw-bold text-danger">+0.17(+0.71%)</p>
+                <p className="fw-bold text-dark">24.12</p>
+                <p className="fw-bold text-success">+0.17(+0.71%)</p>
             </div>
             <div>
                 <h4 className="fw-light text-primary">name 9</h4>
-                <p className="fw-bold text-success">amount</p>
-                <p className="fw-bold text-danger">amount change in %</p>
+                <p className="fw-bold text-dark">amount</p>
+                <p className="fw-bold text-success">amount change in %</p>
             </div>
             <div>
                 <h4 className="fw-light text-primary">name 10</h4>
-                <p className="fw-bold text-success">amount</p>
-                <p className="fw-bold text-danger">amount change in %</p>
+                <p className="fw-bold text-dark">amount</p>
+                <p className="fw-bold text-success">amount change in %</p>
             </div>
             <div>
                 <h4 className="fw-light text-primary">name 11</h4>
                 <p className="fw-bold text-success">amount</p>
-                <p className="fw-bold text-danger">amount change in %</p>
+                <p className="fw-bold text-success">amount change in %</p>
             </div>
             <div>
                 <h4 className="fw-light text-primary">name 12</h4>
